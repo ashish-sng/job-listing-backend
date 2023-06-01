77814b53005bf7c1990642bc88d1618157e4da2f
Ashish Singh <ashishsingh.1899@gmail.com>
2023-06-01 13:07:39 +0530

    initialized server

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+dotenv.config();
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () => {
+  mongoose
+    .connect(process.env.DB_CONNECT, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("MongoDB Connected");
+      console.log(`App listening at http://localhost:${process.env.port}`);
+    })
+    .catch((err) => console.log(err));
+});
