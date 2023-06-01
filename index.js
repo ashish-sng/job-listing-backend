a9852aca976122bb228d98085ea121207a00fb07
Ashish Singh <ashishsingh.1899@gmail.com>
2023-06-01 13:19:11 +0530

    added database config and initialzation

--- index.js
+++ index.js
@@ -13,6 +13,17 @@ dotenv.config();
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
+// Healt check endpoint
+app.get("/health", (req, res) => {
+  const dbStatus =
+    mongoose.connection.readyState === 1 ? "Connected" : "Disconnected";
+
+  res.json({
+    server: "Running",
+    database: dbStatus,
+  });
+});
+
 app.listen(port, () => {
   mongoose
     .connect(process.env.DB_CONNECT, {
